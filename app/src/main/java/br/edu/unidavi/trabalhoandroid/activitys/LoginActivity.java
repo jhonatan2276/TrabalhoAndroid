69bae17b796b15ecae71350446b7eaab4a643424
Jhonatan <jhonatan2276@gmail.com>
2018-05-01 17:06:03 -0300

    Tela de Login Pronta

--- app/src/main/java/br/edu/unidavi/trabalhoandroid/activitys/LoginActivity.java
+++ app/src/main/java/br/edu/unidavi/trabalhoandroid/activitys/LoginActivity.java
@@ -7,6 +7,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import org.greenrobot.eventbus.EventBus;
